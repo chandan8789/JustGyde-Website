9cc616a7d921f18479523e5a223991938dabe76f
Chandan Sharma <chandansharma79928.fea@gmail.com>
2024-06-07 18:53:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/pages/Home/Home";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={Home} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
